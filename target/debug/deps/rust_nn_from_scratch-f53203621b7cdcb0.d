ce80052e90705acf73025539bdeec8660ac5ee9d
Alexis Aoun <alexis.aoun@hotmail.com>
2023-04-14 16:25:24 +0200

    new cargo project

---
+++ target/debug/deps/rust_nn_from_scratch-f53203621b7cdcb0.d
@@ -0,0 +1,5 @@
+/home/alexisaoun/Documents/rust-nn-from-scratch/target/debug/deps/rust_nn_from_scratch-f53203621b7cdcb0.rmeta: src/main.rs
+
+/home/alexisaoun/Documents/rust-nn-from-scratch/target/debug/deps/rust_nn_from_scratch-f53203621b7cdcb0.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/rust_nn_from_scratch.d
@@ -0,0 +1,1 @@
+/home/alexisaoun/Documents/rust-nn-from-scratch/target/debug/rust_nn_from_scratch: /home/alexisaoun/Documents/rust-nn-from-scratch/src/main.rs

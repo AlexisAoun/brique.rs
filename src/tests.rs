fa44a07ada0b9ee8977cc9017fc60005577fc902
Alexis Aoun <alexis.aoun@hotmail.com>
2023-06-01 21:17:40 +0200

    test add row

--- src/tests.rs
+++ src/tests.rs
@@ -1,8 +1,8 @@
 #[cfg(test)]
 mod tests {
     use crate::Matrix;
-    #[test]
-    fn test_dot() {
+
+    fn get_rand_matrix_1() -> Matrix {
         let mut matrix_random_1: Matrix = Matrix::new(4, 2);
         matrix_random_1.data[0][0] = 2.0;
         matrix_random_1.data[0][1] = 1.0;
@@ -12,6 +12,11 @@ mod tests {
         matrix_random_1.data[2][1] = -0.4;
         matrix_random_1.data[3][0] = 0.0;
         matrix_random_1.data[3][1] = 3.0;
+
+        matrix_random_1
+    }
+
+    fn get_rand_matrix_2() -> Matrix {
         let mut matrix_random_2: Matrix = Matrix::new(2, 3);
         matrix_random_2.data[0][0] = 2.0;
         matrix_random_2.data[0][1] = -0.69;
@@ -19,6 +24,23 @@ mod tests {
         matrix_random_2.data[1][0] = -1.0;
         matrix_random_2.data[1][1] = 1.0;
         matrix_random_2.data[1][2] = 0.5;
+
+        matrix_random_2
+    }
+
+    fn get_rand_matrix_3() -> Matrix {
+        let mut matrix_random_3: Matrix = Matrix::new(1, 3);
+        matrix_random_3.data[0][0] = 8.0;
+        matrix_random_3.data[0][1] = -5.21;
+        matrix_random_3.data[0][2] = 0.00;
+
+        matrix_random_3
+    }
+
+    #[test]
+    fn test_dot() {
+        let matrix_random_1 = get_rand_matrix_1();
+        let matrix_random_2 = get_rand_matrix_2();
 
         let output = matrix_random_1.dot(&matrix_random_2);
 
@@ -35,6 +57,25 @@ mod tests {
         expected_output.data[3][0] = -3.0;
         expected_output.data[3][1] = 3.0;
         expected_output.data[3][2] = 1.5;
+
+        assert!(output.is_equal(&expected_output));
+    }
+
+    #[test]
+    fn test_add_row() {
+        let matrix_random_2 = get_rand_matrix_2();
+        let matrix_random_3 = get_rand_matrix_3();
+
+        let output = matrix_random_2.add_value_to_all_rows(&matrix_random_3);
+        let mut expected_output: Matrix =
+            Matrix::new(matrix_random_2.height, matrix_random_2.width);
+
+        expected_output.data[0][0] = 10.0;
+        expected_output.data[0][1] = -5.9;
+        expected_output.data[0][2] = 6.5;
+        expected_output.data[1][0] = 7.0;
+        expected_output.data[1][1] = -4.21;
+        expected_output.data[1][2] = 0.5;
 
         assert!(output.is_equal(&expected_output));
     }

08f576b1f7179e554efa28dd7dac63e8c883808a
Alexis Aoun <alexis.aoun@hotmail.com>
2025-01-18 23:28:23 +0100

    WIP mnist example

---
+++ examples/mnist.rs
@@ -0,0 +1,58 @@
+use brique::layers::*;
+use brique::matrix::*;
+use brique::model::*;
+use brique::optimizer::Optimizer;
+use brique::save_load::*;
+use brique::utils::*;
+
+fn main() {
+    println!("extracting mnist data...");
+    let labels: Matrix = extract_labels("examples/mnist_data/train-labels.idx1-ubyte");
+    let mut images: Matrix = extract_images("examples/mnist_data/train-images.idx3-ubyte");
+    println!("extraction done");
+
+    images.normalize();
+    println!("h {}", images.height);
+    println!("w {}", images.width);
+
+    //_print_a_number(labels, images, 159);
+    //
+
+    let layer1 = Layer::init(28 * 28, 128, true);
+    let layer2 = Layer::init(128, 128, true);
+    let layer3 = Layer::init(128, 10, false);
+
+    let optimizer = Optimizer::Adam {
+        learning_step: 0.01,
+        beta1: 0.9,
+        beta2: 0.999,
+    };
+
+    let mut model = Model::init(vec![layer1, layer2, layer3], optimizer, 0.001);
+
+    model.train(&images, &labels, 128, 5, 1000, false, false);
+    let _ = save_model(&model, "mnist_model3".to_string());
+}
+
+fn _print_a_number(labels: Matrix, images: Matrix, v: usize) {
+    println!("{}", labels.get(0, v));
+
+    for i in 0..28 * 28 {
+        if images.get(v, i) > 0.5 && images.get(v, i) < 1.0 {
+            if i % 28 == 0 {
+                print!("\n");
+            }
+
+            if images.get(v, i) > 0.5 && images.get(v, i) < 0.75 {
+                print!("-");
+            } else {
+                print!("*");
+            }
+        } else {
+            if i % 28 == 0 {
+                print!("\n");
+            }
+            print!("_");
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
-use brique::benchmark::benchmark;
+use brique::{benchmark::benchmark, utils::generate_batch_index};
 
 fn main() {
-    benchmark();
+    //benchmark();
+    //
+    let v = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    let m = generate_batch_index(&v, 4);
+
+    println!("m {:?}", m);
 }
